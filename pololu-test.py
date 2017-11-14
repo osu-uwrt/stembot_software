6f2e6684d668c0afa7305610d0e72e64535f213d
Stembot <osu.uwrt@gmail.com>
2017-11-14 12:08:37 +0000

    Added Pololu library

--- pololu-test.py
+++ pololu-test.py
@@ -1,5 +1,8 @@
 import maestro
+from time import sleep
 servo = maestro.Controller()
 servo.setAccel(0,4)      #set servo 0 acceleration to 4
 servo.setTarget(0,6000)  #set servo to move to center position
+while True:
+	sleep(5)	
 servo.close
