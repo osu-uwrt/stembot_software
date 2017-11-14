f79622fe29756ca029eea75f5e45928df058aeec
Stembot <osu.uwrt@gmail.com>
2017-11-14 07:43:18 +0000

    Working motor test

--- motor-test.py
+++ motor-test.py
@@ -1,68 +1,36 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-
-# pin setup
-PORTSURGE = 8 # white wire
-STBDSURGE = 10 # grey
-PORTHEAVE = 12 # purple
-STBDHEAVE = 16 # green wire
-
-ZERO = 7.5 # number comes from neutral pwm converted to duty cycle
-FREQ = 50
-
-# PWM Setup
-print '   Setting GPIO mode...'
 GPIO.setmode(GPIO.BOARD)
 
-print '   Setting GPIO pins...'
-GPIO.setup(PORTSURGE, GPIO.OUT)
-GPIO.setup(STBDSURGE, GPIO.OUT)
-GPIO.setup(PORTHEAVE, GPIO.OUT)
-GPIO.setup(STBDHEAVE, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(8, GPIO.OUT)
 
 
-print '   Setting PWM frequency...'
-portsurge = GPIO.PWM(PORTSURGE, FREQ)
-stbdsurge = GPIO.PWM(STBDSURGE, FREQ)
-portheave = GPIO.PWM(PORTHEAVE, FREQ)
-stbdheave = GPIO.PWM(STBDHEAVE, FREQ)
+s=GPIO.PWM(10, 50)
+h=GPIO.PWM(12, 50)
+v=GPIO.PWM(8, 50)
+p=GPIO.PWM(16, 50)
 
-print '   Setting PWM duty cycle...'
-portsurge.start(ZERO)
-stbdsurge.start(ZERO)
-portheave.start(ZERO)
-stbdheave.start(ZERO)
+s.start((1500*50)/10000)
+p.start((1500*50)/10000)
+v.start((1500*50)/10000)
+h.start((1500*50)/10000)
+
 sleep(8)
 
+p.ChangeDutyCycle((1600*50)/10000)
+s.ChangeDutyCycle((1600*50)/10000)
+h.ChangeDutyCycle((1600*50)/10000)
+v.ChangeDutyCycle((1600*50)/10000)
 
-# Start up the motors
-
-pwm = 1600
-dc = (FREQ * pwm) / 1000
-
-print "Starting port surge"
-portsurge.ChangeDutyCycle(dc)
 sleep(3)
-portsurge.stop()
 
-print "Starting starboard surge"
-stbdsurge.ChangeDutyCycle(dc)
-sleep(3)
-stbdsurge.stop()
-
-print "Starting port heave"
-portheave.ChangeDutyCycle(dc)
-sleep(3)
-portheave.stop()
-
-print "Starting starboard heave"
-stbdsurge.ChangeDutyCycle(dc)
-sleep(3)
-stbdheave.stop()
-
-print "Test complete"
+p.stop()
+s.stop()
+h.stop()
+v.stop()
 
 GPIO.cleanup()
-
-print "GPIO cleaned up"
