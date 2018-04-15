70fbcaeb097a74b17de0ba1788720096c70f60c0
Aaron Luther <luther.80@osu.edu>
2018-04-15 07:29:59 -0400

    New pololu test

--- pololu-test.py
+++ pololu-test.py
@@ -1,8 +1,8 @@
 import maestro
 from time import sleep
 servo = maestro.Controller()
-servo.setAccel(0,4)      #set servo 0 acceleration to 4
+servo.setAccel(0,0)      #set servo 0 acceleration to 0 (unrestrained)
 servo.setTarget(0,6000)  #set servo to move to center position
 while True:
-	sleep(5)	
+	sleep(5)
 servo.close
