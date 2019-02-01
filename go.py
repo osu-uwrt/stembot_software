b7e85d16ed135003f2a111794baa85cc56cd32c9
blaine141 <blaine141@gmail.com>
2019-02-01 10:32:35 -0500

    better efficiency and usability

--- go.py
+++ go.py
@@ -1,6 +1,7 @@
 import signal
 import socket
 import maestro
+import select
 from time import sleep
 
 PORTSURGE = 0
@@ -11,21 +12,13 @@ AFTHEAVE = 3
 def shutdown(signal, frame):
 
 	stop_thrusters()
-	print('Stopping UDP...')
-	s.close()
-	conn.close()
+	print('Stopping sockets...')
+	for s in conections:
+		s.close()
 	print('Stopping Pololu...')
 	servo.close()
 	print('Exiting...')
 	exit(0)
-
-def wait_for_connect():
-
-	print('Waiting for connection...')
-	global conn
-	conn, addr = s.accept()
-	print('Connected to: ')
-	print(addr)
 
 def stop_thrusters():
 
@@ -39,28 +32,32 @@ def stop_thrusters():
 	servo.setTarget(FWDHEAVE,6000)
 	servo.setAccel(AFTHEAVE,0)
 	servo.setTarget(AFTHEAVE,6000)
+	
+def send_voltage(voltage):
+	
+	for s in conections:
+		if s != incomingConnection:
+			s.sendall(str(voltage).encode())
 
-
+# Subscribe shutdown
 signal.signal(signal.SIGINT, shutdown)
 
-print('Waiting for startup')
-sleep(30)
-print('Setting UDP mode...')
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Setting up socket...')
+incomingConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 connected = False
-
 while not connected:
 	try:
-		s.bind(('192.168.1.112', 50000))
+		incomingConnection.bind(('0.0.0.0', 50000))
 		connected = True
 	except:
 		sleep(1)
+incomingConnection.listen(1);
+conections = [incomingConnection]
 
-s.listen(1);
 
 print('Connecting to maestro...')
-
 connected = False
 while not connected:
 	try:
@@ -70,30 +67,39 @@ while not connected:
 		sleep(1)
 
 
-
 stop_thrusters()
-print('Waiting for thrusters to initialize...')
-sleep(5)
-
 print('STEMbot is online')
 
-wait_for_connect()
-
 while 1:
+	readable, writable, exceptional = select.select(conections, [], conections, 0)
 
-	try:
-		data = conn.recv(8)
-		conn.send('0')
-	except:
-		stop_thrusters()
-		wait_for_connect()
-		continue
+	for s in readable:
+		if s == incomingConnection:
+			conn, addr = incomingConnection.accept()
+			print("Connected to "+str(addr))
+			conections.append(conn)
+		else:
+			try:
+				data = s.recv(1024)
+			except:
+				conections.remove(s)
+				stop_thrusters()
+				print("Lost connection")
+				continue
+			if not data:
+				conections.remove(s)
+				stop_thrusters()
+				print("Lost connection")
+				continue
+			servo.setTarget(PORTSURGE, ord(data[0]) * 256 + ord(data[1]))
+			servo.setTarget(STBDSURGE, ord(data[2]) * 256 + ord(data[3]))
+			servo.setTarget(FWDHEAVE, ord(data[4]) * 256 + ord(data[5]))
+			servo.setTarget(AFTHEAVE, ord(data[6]) * 256 + ord(data[7]))
+			
+	#Read Voltage
+	voltage = 0
+	send_voltage(voltage)
+	sleep(0.001)
 
-	if not data:
-		continue
-
-	servo.setTarget(PORTSURGE, ord(data[0]) * 256 + ord(data[1]))
-	servo.setTarget(STBDSURGE, ord(data[2]) * 256 + ord(data[3]))
-	servo.setTarget(FWDHEAVE, ord(data[4]) * 256 + ord(data[5]))
-	servo.setTarget(AFTHEAVE, ord(data[6]) * 256 + ord(data[7]))
+	
 	
