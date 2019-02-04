256689624a1bf0f234a235ceb30debdcfcf8eadd
blaine141 <blaine141@gmail.com>
2019-02-04 04:43:44 -0500

    Made controller non-blocking

--- STEMbot-driver.py
+++ STEMbot-driver.py
@@ -12,6 +12,7 @@ def shutdown(signal, frame):
 	s.close()
 	print('Exiting...')
 	exit(0)
+signal.signal(signal.SIGINT, shutdown)
 
 # Set thruster percentages
 # About 6% is deadzone
@@ -34,51 +35,49 @@ def rectify_axis(byte):
         byte += 255
     return byte
 
-signal.signal(signal.SIGINT, shutdown)
+def print_voltage(value):
+	value=int(value.split("\n")[-2])
+	percent=(value*12.1/867-11.1)/1.5
+	percent=str(round(percent*100,2))
+	print("Battery: " + percent+"%")
+
+
 
 controller = open('/dev/input/js0','r')
+print('Connecting to STEMbot...')
+stembotConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+stembotConn.connect(('192.168.1.112', 50000))
+print('Connected!')
+
+packet = []
+heave = 0
+surge = 0
+turn = 0
 
 while 1:
-	print('Connecting to STEMbot...')
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(('192.168.1.112', 50000))
-	print('Connected!')
+	readable, writable, exceptional = select.select([stembotConn, controller], [], [], 0)
+	
+	for a in readable:
+		if a == stembotConn: # If stembot activity
+			data = stembotConn.recv(1024)
+			print_voltage(data.encode())
 
-	packet = []
-
-	heave = 0
-	surge = 0
-	turn = 0
-
-	while 1:
-		
-		readable, writable, exceptional = select.select([s], [], [s], 0)
-		
-		for socket in readable:
-			data = socket.recv(1024)
-			if not data:
-				socket.close()
-				break
-			print(data.encode())
-			
-		packet += [int(str(hex(ord(controller.read(1)))), 16)]
-		
-		# Process Packet
-		if len(packet) == 8:
-			# Is it a Stick?
-			if packet[6] == 2:
-				# Right Stick X Update
-				if packet[7] == 3:
-					turn = (rectify_axis(packet[5])-128)/128.0
-				# Right Stick Y Update
-				elif packet[7] == 4:
-					surge = (rectify_axis(packet[5])-128)/128.0
-				# Left Stick Y Update
-				elif packet[7] == 1:
-					heave = (rectify_axis(packet[5])-128)/128.0
-			packet = []
-			
-			set_thrusters((surge - turn) *50, (surge + turn) * 50, heave*100, heave * 100)
+		if a == controller: # If controller data
+			packet += [int(str(hex(ord(controller.read(1)))), 16)]
+			if len(packet) == 8:
+				# Is it a Stick?
+				if packet[6] == 2:
+					# Right Stick X Update
+					if packet[7] == 3:
+						turn = (rectify_axis(packet[5])-128)/128.0
+					# Right Stick Y Update
+					elif packet[7] == 4:
+						surge = (rectify_axis(packet[5])-128)/128.0
+					# Left Stick Y Update
+					elif packet[7] == 1:
+						heave = (rectify_axis(packet[5])-128)/128.0
+				packet = []
+				set_thrusters((surge - turn) *25, (surge + turn) * 25, heave*25, heave * 25)
 	
 
                
